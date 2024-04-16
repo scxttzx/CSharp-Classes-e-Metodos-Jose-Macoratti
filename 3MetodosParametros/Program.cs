177381d0c948482143a94f8b3d5321b2ddabca67
crfmattos <gabrielmbserra@gmail.com>
2024-04-15 23:46:32 -0300

    Aulas 23/06/2024

---
+++ 3MetodosParametros/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("## Métodos com Parametros ##");
+
+MinhaClasse minhaClasse = new MinhaClasse();
+minhaClasse.Saudacao("Maria", DateTime.Now.ToShortDateString());
+
+Console.ReadKey();
+
+public class MinhaClasse
+{
+    public void Saudacao(string nome, string data)
+    {
+        Console.WriteLine(nome);
+        Console.WriteLine(data);
+    }
+}
---
+++ 4MetodosParametros/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("## Métodos - parâmetros ##");
+
+Aluno aluno = new Aluno();
+aluno.Consultar();
+
+Console.ReadKey();
+
+public class Aluno
+{
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+
+    public void Consultar()
+    {
+        var aluno = new Aluno();
+
+        Console.WriteLine("Nome: ");
+        aluno.Nome = Console.ReadLine();
+
+        Console.WriteLine("Idade: ");
+        aluno.Idade = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Sexo: ");
+        aluno.Sexo = Console.ReadLine();
+
+        Console.WriteLine("Aprovado (S)im ou (N)ão: ");
+        aluno.Aprovado = Console.ReadLine();
+
+        Curso curso = new Curso();
+        curso.Resultado(aluno);
+    }
+}
+
+public class Curso
+{
+    public void Resultado(Aluno aluno)
+    {
+        Console.WriteLine($"O aluno {aluno.Nome}, sexo {aluno.Sexo} com {aluno.Idade} anos");
+        if (aluno.Aprovado != "S")
+        {
+            Console.WriteLine("foi reprovado");
+        } else
+        {
+            Console.WriteLine("foi aprovado");
+        }
+    }
+}
