e5928fbb1c65377a6bcd88124e4b50f76bb6279e
crfmattos <gabrielmbserra@gmail.com>
2024-04-16 14:06:01 -0300

    16/04/2024 - trabalho

---
+++ 5Construtores/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("## Construtores ##\n");
+
+
+Aluno aluno = new Aluno("Maria", 22, "feminino", "S");
+
+// Operador (condição ? expressão1_se_true : expressão2_se_false)
+Console.WriteLine(aluno.Nome == null ? "null" : aluno.Nome);
+Console.WriteLine(aluno.Idade == 0 ? "zero" : aluno.Idade);
+Console.WriteLine(aluno.Sexo == null ? "null" : aluno.Sexo);
+Console.WriteLine(aluno.Aprovado == null ? "null" : aluno.Aprovado);
+
+Console.ReadKey();
+
+public class Aluno
+{
+    public Aluno(string nome, int idade, string sexo, string aprovado)
+    {
+        Nome = nome;
+        Idade = idade;
+        Sexo = sexo;
+        Aprovado = aprovado;
+    }
+
+    public string? Nome;
+    public int Idade;
+    public string? Sexo;
+    public string? Aprovado;
+}
