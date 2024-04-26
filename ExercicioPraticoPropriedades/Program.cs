c90f68a7da49c0143ffc33c7bad5ed1a245d15f7
crfmattos <gabrielmbserra@gmail.com>
2024-04-26 14:22:03 -0300

    Aulas 26/04/2024 - 14:21 - Job

---
+++ ExercicioPraticoPropriedades/Program.cs
@@ -0,0 +1,54 @@
+﻿Console.WriteLine("## Propriedades ##\n");
+
+Produto p1 = new();
+p1.Nome = "Caderno Espiral";
+p1.Preco = 4.99;
+//p1.Desconto = 0.05;
+//p1.PrecoFinal = p1.Preco - (p1.Preco * p1.Desconto);
+p1.EstoqueMinimo = 10;
+
+p1.Exibir();
+
+Console.ReadKey();
+
+public class Produto
+{
+    private string? nome;
+    public string? Nome 
+    {
+        get { return nome.ToUpper(); }
+        set { nome = value; } 
+    }
+    private double preco;
+    public double Preco 
+    { 
+        get { return preco; }
+        set 
+        {
+            if (value < 5)
+                preco = 5.00;
+            else
+                preco = value;
+        } 
+    }   
+    private double desconto = 0.05;
+    public double Desconto 
+    {
+        get { return desconto; }
+    }
+    public double PrecoFinal 
+    { 
+        get { return Preco - (Preco * Desconto); }
+    }
+    private int minimo;
+    public int EstoqueMinimo 
+    { 
+        set { minimo = value; }
+    }
+
+    public void Exibir()
+    {
+        Console.WriteLine($"{Nome} \n{Preco.ToString("c")} \n{Desconto*100}%" +
+                          $" \n{PrecoFinal.ToString("c")} \nRestantes: {minimo}");
+    }
+}
