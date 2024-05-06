c2c3c277c7a7ede05f22d9e313c2fa860f97b5d8
crfmattos <gabrielmbserra@gmail.com>
2024-05-05 21:29:10 -0300

    exercicios 05/05/2024 - 21:29 -  Home

---
+++ TiposAnonimos/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.WriteLine("## Tipos Anônimos ##");
+
+var aluno = new
+{
+    Nome = "Maria",
+    Idade = 43,
+    Email = "jairo@email.com",
+    Endereco = new { ID = 1, Cidade = "Santos", Pais = "Brasil" }
+};
+
+//aluno.Idade = 43; 
+//Não pode pois a propriedade é de apenas leitura
+
+Console.WriteLine(aluno.GetType().ToString());
+
+Console.WriteLine(aluno.Nome + " " + aluno.Idade);
+Console.WriteLine(aluno.Nome);
+Console.WriteLine(aluno.Endereco.Cidade);
+
+
+var aluno1 = new[]
+{
+    new { Nome = "João", Idade = 43, Email = "jairo@email.com" }
+    new { Nome = "Maria", Idade = 3, Email = "asd@email.com" }
+    new { Nome = "José", Idade = 21, Email = "tt@email.com" }
+   
+};
+
+Console.WriteLine(aluno1[0].Nome);
+
+Console.ReadKey();
